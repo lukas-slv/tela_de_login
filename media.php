1b2cfa5c29f3d4fa384c7d033c1d92ad8d7eda5f
lukas-slv <lukasbatera115@gmail.com>
2024-11-22 12:00:40 -0300

    atualização php

---
+++ media.php
@@ -0,0 +1,6 @@
+!-- Formulário de envio de dados --
+
+Olá, Usuário! Seu login foi realizado com sucesso!
+Seu login foi:  <?php echo $_POST["login"]; ?>
+Sua senha foi:  <?php echo $_POST["senha"]; ?>
+
